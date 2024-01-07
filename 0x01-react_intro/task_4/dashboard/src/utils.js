eeebae2b0019492eb5dbe57a04699c64821e8690
Jeffrey-Otu <otujeffrey02@gmail.com>
2024-01-06 19:26:40 -0800

    readme

---
+++ 0x01-react_intro/task_4/dashboard/src/utils.js
@@ -0,0 +1,7 @@
+const getFullYear = () => new Date().getFullYear();
+
+const getFooterCopy = (isIndex) => {
+    return isIndex === true ? 'Holberton School' :  'Holberton School main dashboard'
+}
+
+export { getFullYear, getFooterCopy }
